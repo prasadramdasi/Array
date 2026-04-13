9fb760f4faf11204b94a4fba645f452f36c63d72
@prasad <99307972+prasadramdasi@users.noreply.github.com>
2026-04-13 16:12:10 +0530

    Write a java program to check if 2 arrays are equal or not.
     Write a java program to check if 2 arrays are equal or not. 
    Array 1: [1, 2, 3, 4, 5], Array 2: [1, 2, 3, 4, 5] 
    Output: true 
    Array 1: [7, 14, 21,45,35], Array 2: [7, 14, 21, 36] 
    Output: false

---
+++ equalArray.java
@@ -0,0 +1,11 @@
+    public static boolean isEqual(int a[],int b[]){
+
+        for(int i=0;i<a.length;i++){
+
+                if(a[i]!=b[i]){
+                    return false;
+
+                }
+        }
+        return true;
+    }
